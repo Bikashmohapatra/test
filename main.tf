87568f0466c149654b1d799deabe229be068a26f
Bikashmohapatra <bikashmohapatrajob@gmail.com>
2025-12-10 20:11:59 +0530

    Initial clean commit without .terraform

---
+++ main.tf
@@ -0,0 +1,33 @@
+
+# Create IAM user
+resource "aws_iam_user" "demo_user" {
+  name = "test-console-user"
+  force_destroy = true
+}
+
+# Attach a managed policy (e.g., AdministratorAccess for demo)
+# 👉 In production, use least privilege instead!
+#resource "aws_iam_user_policy_attachment" "demo_attach" {
+#  user       = aws_iam_user.demo_user.name
+#  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+#}
+
+# Create login profile (password for console access)
+
+resource "aws_iam_access_key" "demo_key" {
+  user = aws_iam_user.demo_user.name
+}
+
+output "access_key_id" {
+  value = aws_iam_access_key.demo_key.id
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.demo_key.secret
+  sensitive = true
+}
+
+# Output username
+output "iam_username" {
+  value = aws_iam_user.demo_user.name
+}
