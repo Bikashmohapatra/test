974ee8809c402bde0f9e85fff4735e3bd403bf59
Bikashmohapatra <bikashmohapatrajob@gmail.com>
2025-12-21 19:18:01 +0530

    create ec2

--- new.tf
+++ new.tf
@@ -1,15 +1,15 @@
-# module "ec2_instance" {
-#   source = "terraform-aws-modules/ec2-instance/aws"
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-#   name = "single-instance"
+  name = "single-instance"
 
-#   instance_type = "t3a.small"
-#   key_name      = "test"
-#   monitoring    = false
-#   subnet_id     = "subnet-0ed5d2b88c1e41ad5"
+  instance_type = "t3a.small"
+  key_name      = "test"
+  monitoring    = false
+  subnet_id     = "subnet-0ed5d2b88c1e41ad5"
 
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "test"
-#   }
-# }
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
+  }
+}
