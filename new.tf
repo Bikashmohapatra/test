dd4631d7fb9a5df2d9d84d95884b43422f05ba4e
Bikashmohapatra <bikashmohapatrajob@gmail.com>
2025-12-22 19:19:23 +0530

    spin up ec2

--- new.tf
+++ new.tf
@@ -1,16 +1,16 @@
-# #hello there hi
-# module "ec2_instance" {
-#   source = "terraform-aws-modules/ec2-instance/aws"
+#hello there hi
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-#   name = "single-instance"
+  name = "single-instance"
 
-#   instance_type = "t3a.small"
-#   key_name      = "test"
-#   monitoring    = false
-#   subnet_id     = "subnet-0ed5d2b88c1e41ad5"
+  instance_type = "t3a.small"
+  key_name      = "test"
+  monitoring    = false
+  subnet_id     = "subnet-0ed5d2b88c1e41ad5"
 
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "test"
-#   }
-# }
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
+  }
+}
