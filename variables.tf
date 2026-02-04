0684afd84e596b5cfaa7ad14c98e4fedd8d3b529
Bikashmohapatra <bikashmohapatrajob@gmail.com>
2026-02-04 20:44:16 +0530

    create EKS cluster

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "iam_username" {
-  description = "The name of the IAM user to create"
-  type        = set(string)
-  default     = ["bikash", "miki", "rinky", "benudhar", "bandana"]
-  
-}
+# variable "iam_username" {
+#   description = "The name of the IAM user to create"
+#   type        = set(string)
+#   default     = ["bikash", "miki", "rinky", "benudhar", "bandana"]
+
+# }
