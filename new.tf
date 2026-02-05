cff9d8ba7df567135a8e14228206dd87cccad68a
Bikashmohapatra <bikashmohapatrajob@gmail.com>
2026-02-05 13:49:38 +0530

    increasing the disk space and adding jenkins installation steps

--- new.tf
+++ new.tf
@@ -8,6 +8,11 @@ module "ec2_instance" {
   key_name      = "test"
   monitoring    = false
   subnet_id     = "subnet-0ed5d2b88c1e41ad5"
+  root_block_device = [
+    {
+      volume_size = 30
+    }
+  ]
   ############################
   # Enable SSM Login
   #############################
@@ -49,6 +54,13 @@ module "ec2_instance" {
               # Update system
               sudo dnf update -y
 
+              # Install Jenkins
+              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+              sudo dnf install jenkins -y
+              sudo systemctl start jenkins
+              sudo systemctl enable jenkins
+
               # Install Docker
               sudo dnf install docker -y
               sudo systemctl start docker
@@ -63,21 +75,21 @@ module "ec2_instance" {
               sudo mv kubectl /usr/local/bin/kubectl
 
               # Verify kubectl
-              kubectl version --client
+              #kubectl version --client
 
               # Install Minikube
-              curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-              chmod +x minikube-linux-amd64
-              sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+              #curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+              #chmod +x minikube-linux-amd64
+              #sudo mv minikube-linux-amd64 /usr/local/bin/minikube
 
               # Verify Minikube
-              minikube version
+              #minikube version
 
               # Start Minikube using Docker driver
-              sudo -u ec2-user minikube start --driver=docker
+              #sudo -u ec2-user minikube start --driver=docker
 
               # Check Minikube status
-              sudo -u ec2-user minikube status
+              #sudo -u ec2-user minikube status
               EOF
 
   tags = {
